b1f58a5627dd0fb2270e1f7db7af9504dd0d7fc9
Louise Moxhay <louisemoxy@gmail.com>
2023-06-27 15:02:40 +0100

    Add query parm link and tidy up

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         <a href={'https://example.com/'} target={'_blank'} rel='noreferrer'>
           <p className={'link'} id={'link'}>Link to Example webpage</p>
         </a>
+        <a href={'#updated'}>
+          <p className={'link'} id={'queryParam'}>Click to update queryParams</p>
+        </a>
       </header>
     </div>
   );
