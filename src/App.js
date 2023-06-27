729c54dea5b777e51747cdd6e86a7123b19c15dc
Louise Moxhay <louisemoxy@gmail.com>
2023-06-27 14:24:19 +0100

    Create link from testing website

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <h1>Testing Website</h1>
+        <a href={'https://example.com/'} target={'_blank'}>
+          <p className={'link'} id={'link'}>Link to Example webpage</p>
         </a>
       </header>
     </div>
